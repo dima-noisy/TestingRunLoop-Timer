68deb7faa84dce5461b4958f13f111f667947710
Dmitry Poryadin <dporyadin11@gmail.com>
2024-10-19 12:23:56 +0300

    Initial Commit

---
+++ RunLoop,TimerTests/RunLoop_TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RunLoop_TimerTests.swift
+//  RunLoop,TimerTests
+//
+//  Created by Дмитрий Порядин on 19.10.2024.
+//
+
+import Testing
+@testable import RunLoop_Timer
+
+struct RunLoop_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
